8bd6764be698568badaee527d6d0e240c2221aee
habi <466039454@qq.com>
2024-11-14 17:48:12 +0800

    update image aligh

--- main.py
+++ main.py
@@ -7,24 +7,119 @@
 @Date    ：2024/11/12 15:31 
 '''
 from fastapi import FastAPI,Body,HTTPException
-from pydantic import BaseModel
 import requests
 from io import BytesIO
 from PIL import Image
 
-from arcface_detect import getfacesim
+#from arcface_detect import getfacesim
 import cv2
 import numpy as np
-#import face_alignment
-# Create a FastAPI instance
 app = FastAPI()
 
-# @app.on_event("startup")
-# async def startup_event():
-#     global fa
-#     # 初始化人脸对齐方法
-#     fa = face_alignment.FaceAlignment(face_alignment.LandmarksType.TWO_D, device='cpu')
+from  arcface.engine import *
 
+from config import  APPID,SDKKey
+
+@app.on_event("startup")
+async def startup_event():
+    # 激活接口,首次需联网激活
+    global face_engine
+    res = ASFOnlineActivation(APPID, SDKKey)
+    if (MOK != res and MERR_ASF_ALREADY_ACTIVATED != res):
+        print("ASFActivation fail: {}".format(res))
+    else:
+        print("ASFActivation sucess: {}".format(res))
+
+    # 获取激活文件信息
+    res, activeFileInfo = ASFGetActiveFileInfo()
+
+    if (res != MOK):
+        print("ASFGetActiveFileInfo fail: {}".format(res))
+    else:
+        print(f"{activeFileInfo}")
+
+    # 获取人脸识别引擎
+    face_engine = ArcFace()
+
+    # 需要引擎开启的功能
+    mask = ASF_FACE_DETECT | ASF_FACERECOGNITION
+
+    # 初始化接口
+    res = face_engine.ASFInitEngine(ASF_DETECT_MODE_IMAGE, ASF_OP_0_ONLY, 30, 10, mask)
+    if (res != MOK):
+        print("ASFInitEngine fail: {}".format(res))
+    else:
+        print("ASFInitEngine sucess: {}".format(res))
+
+def getfacesim(img1, img2):
+    orientations = [ASF_OP_0_ONLY, ASF_OP_90_ONLY, ASF_OP_180_ONLY, ASF_OP_270_ONLY]
+    face_feature1 = None
+    face_feature2 = None
+
+    # 检测第一张图中的人脸
+    for i, orientation in enumerate(orientations):
+        print(f"Trying orientation {i + 1}/{len(orientations)} for the first image.")
+
+        res = face_engine.ASFInitEngine(ASF_DETECT_MODE_IMAGE, orientation, 30, 10, mask)
+        if res != MOK:
+            print("ASFInitEngine fail for orientation {}: {}".format(orientation, res))
+            return None
+
+        res, detectedFaces1 = face_engine.ASFDetectFaces(img1)
+        if res == MOK and detectedFaces1.faceRect:
+            single_detected_face1 = ASF_SingleFaceInfo()
+            single_detected_face1.faceRect = detectedFaces1.faceRect[0]
+            single_detected_face1.faceOrient = detectedFaces1.faceOrient[0]
+            res, face_feature1 = face_engine.ASFFaceFeatureExtract(img1, single_detected_face1)
+            if res == 90127:
+                print("Detected specific error code 90127, skipping further attempts for the second image.")
+                break
+            if res == MOK:
+                break  # 成功提取特征，退出循环
+            else:
+                print("ASFFaceFeatureExtract 1 fail: {}".format(res))
+        else:
+            print("ASFDetectFaces 1 fail for orientation {}: {}".format(orientation, res))
+
+    if face_feature1 is None:
+        print("No valid face feature extracted from the first image.")
+        return None
+
+    # 检测第二张图中的人脸
+    for i, orientation in enumerate(orientations):
+        print(f"Trying orientation {i + 1}/{len(orientations)} for the second image.")
+
+        res = face_engine.ASFInitEngine(ASF_DETECT_MODE_IMAGE, orientation, 30, 10, mask)
+        if res != MOK:
+            print("ASFInitEngine fail for orientation {}: {}".format(orientation, res))
+            return None
+
+        res, detectedFaces2 = face_engine.ASFDetectFaces(img2)
+
+        # 检查特定错误代码
+        if res == 90127:
+            print("Detected specific error code 90127, skipping further attempts for the second image.")
+            break
+
+        if res == MOK and detectedFaces2.faceRect:
+            single_detected_face2 = ASF_SingleFaceInfo()
+            single_detected_face2.faceRect = detectedFaces2.faceRect[0]
+            single_detected_face2.faceOrient = detectedFaces2.faceOrient[0]
+            res, face_feature2 = face_engine.ASFFaceFeatureExtract(img2, single_detected_face2)
+            if res == MOK:
+                break  # 成功提取特征，退出循环
+            else:
+                print("ASFFaceFeatureExtract 2 fail: {}".format(res))
+        else:
+            print("ASFDetectFaces 2 fail for orientation {}: {}".format(orientation, res))
+
+    if face_feature2 is None:
+        print("No valid face feature extracted from the second image.")
+        return None
+
+    # 比较两个人脸的相似度
+    res, score = face_engine.ASFFaceFeatureCompare(face_feature1, face_feature2)
+    return score  # 返回相似度得分
 def read_image_from_url(url):
     # 这里是读取图像的函数，具体实现根据你的需求来
     # 例如，可以使用 requests 库来获取图像并使用 cv2 读取
@@ -33,18 +128,6 @@ def read_image_from_url(url):
     img = Image.open(BytesIO(response.content))
     return cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
 
-def align_face(image, landmarks):
-    left_eye = landmarks[36:42]  # 左眼关键点
-    right_eye = landmarks[42:48]  # 右眼关键点
-    left_eye_center = np.mean(left_eye, axis=0)
-    right_eye_center = np.mean(right_eye, axis=0)
-    dy = right_eye_center[1] - left_eye_center[1]
-    dx = right_eye_center[0] - left_eye_center[0]
-    angle = np.arctan2(dy, dx) * 180.0 / np.pi
-    center = (image.shape[1] // 2, image.shape[0] // 2)
-    M = cv2.getRotationMatrix2D(center, angle, scale=1.0)
-    aligned_image = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
-    return aligned_image
 
 # Define a Pydantic model for the request body
 
@@ -62,22 +145,6 @@ def post_facesim(
         # 检测第一张图中的人脸
         img1_ori = read_image_from_url(image1)
         img2_ori = read_image_from_url(image2)
-        # preds1 = fa.get_landmarks(img1_ori)
-        # if preds1 is not None:
-        #     # 如果检测到人脸，进行对齐
-        #     aligned_img1 = align_face(img1_ori, preds1[0])
-        # else:
-        #     print("第一张图未检测到人脸！")
-        #     return None
-
-        # # 检测第二张图中的人脸
-        # preds2 = fa.get_landmarks(img2_ori)
-        # if preds2 is not None:
-        #     # 如果检测到人脸，进行对齐
-        #     aligned_img2 = align_face(img2_ori, preds2[0])
-        # else:
-        #     print("第二张图未检测到人脸！")
-        #     return None
         result = getfacesim(img1_ori, img2_ori)
         print(f"{image1} and {image2} sim is {result}")
         return {
