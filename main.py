29796f36ab20b478b9becad779277eff8df6e6ca
habi <466039454@qq.com>
2025-03-04 15:16:11 +0800

    update url replace

--- main.py
+++ main.py
@@ -127,8 +127,8 @@ async def post_facesim(
         img1_ori, img2_ori = None, None  # 先初始化变量，防止 UnboundLocalError
         num1 = 0
         num2 = 0
-        num1,img1_ori = find_faces_by_rotation(image1)
-        num2,img2_ori = find_faces_by_rotation(image2)
+        num1,img1_ori = find_faces_by_rotation(image1_url)
+        num2,img2_ori = find_faces_by_rotation(image2_url)
         if num1 != 1 and num2 != 1:
             return {
                 "code": 200,
